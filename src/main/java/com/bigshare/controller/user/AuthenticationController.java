e88d096c5bb7c1c387578b41788700acfd122e51
Maksym7700 <maxkuziv7@gmail.com>
2024-12-14 17:56:58 +0200

    Added test endpoint

--- src/main/java/com/bigshare/controller/user/AuthenticationController.java
+++ src/main/java/com/bigshare/controller/user/AuthenticationController.java
@@ -4,6 +4,7 @@ import com.bigshare.requests.AuthenticationRequest;
 import com.bigshare.service.user.UserDetailServiceImpl;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -26,6 +27,11 @@ public class AuthenticationController {
     public ResponseEntity<?> login(@RequestBody @Valid AuthenticationRequest authenticationRequest) {
 
         return userDetailService.login(authenticationRequest);
+    }
+
+    @GetMapping("/ping")
+    public String pong() {
+        return "pong";
     }
 
 }
